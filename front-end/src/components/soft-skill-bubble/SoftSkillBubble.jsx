224eec4582b66f80856a2439e5218815ce38b0fe
Maelle <maelle.nioche@gmail.com>
2024-02-05 09:18:35 +0100

    add soft skills

--- front-end/src/components/soft-skill-bubble/SoftSkillBubble.jsx
+++ front-end/src/components/soft-skill-bubble/SoftSkillBubble.jsx
@@ -1,2 +1,6 @@
-function SoftSkillBubble() {}
+function SoftSkillBubble(props) {
+  const color = props.color === 0 ? 'blue' : 'red';
+
+  return <div className={`bubble ${color}`}>{props.children}</div>;
+}
 export default SoftSkillBubble;
--- front-end/src/sections/soft-skills/SoftSkills.jsx
+++ front-end/src/sections/soft-skills/SoftSkills.jsx
@@ -1,7 +1,52 @@
+import { useState, useEffect } from 'react';
+import datas from '../../assets/datas.json';
+
+import SoftSkillBubble from '../../components/soft-skill-bubble/SoftSkillBubble';
+
 function SoftSkills() {
+  const softSkills = datas.softSkills.skills;
+  const svg = datas.softSkills.logo;
+
+  const [selectedSkill1, setSelectedSkill1] = useState([]);
+  const [selectedSkill2, setSelectedSkill2] = useState([]);
+
+  const selectRandomSkills = () => {
+    const shuffledSkill = softSkills.sort(() => Math.random() - 0.5);
+    const selectedSet1 = shuffledSkill.slice(0, 2);
+    const selectedSet2 = shuffledSkill.slice(2, 4);
+    setSelectedSkill1(selectedSet1);
+    setSelectedSkill2(selectedSet2);
+  };
+
+  // Utilisation de useEffect pour déclencher la fonction toutes les 5 secondes
+  useEffect(() => {
+    const interval = setInterval(() => {
+      selectRandomSkills();
+    }, 5000); // Déclenchement toutes les 5 secondes
+
+    return () => clearInterval(interval); // Nettoyage de l'intervalle lors du démontage du composant
+  }, []);
+
   return (
-    <section>
+    <section id="soft-skills">
       <h2>Mon savoir-être</h2>
+      <div className="details">
+        <div className="bubble-column left">
+          {selectedSkill1.map((skill, index) => (
+            <SoftSkillBubble color={index} key={index}>
+              {skill}
+            </SoftSkillBubble>
+          ))}
+        </div>
+        <img onClick={selectRandomSkills} src={svg} alt="" />
+        <div className="bubble-column right">
+          {selectedSkill2.map((skill, index) => (
+            <SoftSkillBubble color={index} key={index}>
+              {skill}
+            </SoftSkillBubble>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
