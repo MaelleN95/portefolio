098ca7bc663b5a4749b334ee4b2a720977bb644c
Maelle <maelle.nioche@gmail.com>
2024-02-10 23:47:09 +0100

    update : modification of links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,32 @@
+import { Link, useParams } from 'react-router-dom';
+
 import { Anchor, ConfigProvider } from 'antd';
 
 import datas from '../../assets/datas.json';
 
 function Header() {
+  let { projectsId } = useParams();
+
   return (
     <header>
-      <img src={datas.logo} alt="icône Maelle Nioche" width="40" height="40" />
+      {projectsId ? (
+        <Link to="/" title={`Lien vers la page d'accueil`}>
+          <img
+            src={datas.logo}
+            alt="icône Maelle Nioche"
+            width="40"
+            height="40"
+          />
+        </Link>
+      ) : (
+        <img
+          src={datas.logo}
+          alt="icône Maelle Nioche"
+          width="40"
+          height="40"
+        />
+      )}
+
       <ConfigProvider
         theme={{
           token: {
@@ -18,20 +39,37 @@ function Header() {
           },
         }}
       >
-        <Anchor
-          type="primary"
-          direction="horizontal"
-          targetOffset="70"
-          items={[
-            { key: 'about-me-1', href: '#about-me', title: 'A propos' },
-            {
-              key: 'my-projects-1',
-              href: '#my-projects',
-              title: 'Mes projets',
-            },
-            { key: 'contact-1', href: '#contact', title: 'Contact' },
-          ]}
-        />
+        {projectsId ? (
+          <Anchor
+            type="primary"
+            direction="horizontal"
+            targetOffset="70"
+            items={[
+              { key: 'about-me-1', href: '/#about-me', title: 'A propos' },
+              {
+                key: 'my-projects-1',
+                href: '/#my-projects',
+                title: 'Mes projets',
+              },
+              { key: 'contact-1', href: '/#contact', title: 'Contact' },
+            ]}
+          />
+        ) : (
+          <Anchor
+            type="primary"
+            direction="horizontal"
+            targetOffset="70"
+            items={[
+              { key: 'about-me-1', href: '#about-me', title: 'A propos' },
+              {
+                key: 'my-projects-1',
+                href: '#my-projects',
+                title: 'Mes projets',
+              },
+              { key: 'contact-1', href: '#contact', title: 'Contact' },
+            ]}
+          />
+        )}
       </ConfigProvider>
     </header>
   );
