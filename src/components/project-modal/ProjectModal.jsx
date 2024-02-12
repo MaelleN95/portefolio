0a5bcf5449358a000b2c47873d0ecb8f3bff2451
Maelle <maelle.nioche@gmail.com>
2024-02-13 00:08:00 +0100

    add : responsive design for small mobil

--- src/components/project-modal/ProjectModal.jsx
+++ src/components/project-modal/ProjectModal.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import useScreenSize from '../../lib/customHooks';
 
 import Modal from 'antd/es/modal/Modal';
 
@@ -12,12 +13,28 @@ export function ProjectModal(props) {
     setIsModalOpen(false);
   };
 
+  const screenWidth = useScreenSize().width;
+
+  const [modalWidth, setModalWidth] = useState('88');
+
+  useEffect(() => {
+    if (screenWidth <= 1175) {
+      setModalWidth('95');
+    } else {
+      setModalWidth('88');
+    }
+  }, [screenWidth]);
   return (
     <>
       <button type="primary" onClick={showModal} className="button-modale">
         Voir plus d&apos;images
       </button>
-      <Modal open={isModalOpen} footer={[]} onCancel={handleCancel} width="80%">
+      <Modal
+        open={isModalOpen}
+        footer={[]}
+        onCancel={handleCancel}
+        width={`${modalWidth}%`}
+      >
         {pictures.map((pic, i) => (
           <img src={pic} alt="image des pages du projet" key={i} />
         ))}
--- src/lib/customHooks.jsx
+++ src/lib/customHooks.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export default function useWindowSize() {
+export default function useScreenSize() {
   const isClient = typeof window === 'object';
 
   function getSize() {
